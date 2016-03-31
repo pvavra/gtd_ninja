9ca85a6c239b94e530b53c2fcf6e9967244cf053
Peter Vavra <peter.vavra.1984@gmail.com>
2016-03-31 22:07:21 +0200

    improve statis pages and add better test-gem

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   
   root  'static_pages#home'
 
-  get 'static_pages/help'
-  get 'static_pages/home'
+  get 'home' => 'static_pages#home'
+  get 'about' => 'static_pages#about'
 
   get 'users/new'
 
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,22 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  def setup
+    @base_title = "GTD Ninja App"
+  end
+
+
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title",  "Home | #{@base_title}"
   end
 
-  test "should get help" do
-    get :help
+  test "should get about" do
+    get :about
     assert_response :success
+    assert_select "title",  "About | #{@base_title}"
   end
+
 
 end
